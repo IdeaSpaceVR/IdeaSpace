a182dd1466c0d1307dad2578018c4a9e5cd3e7f0
Chris Car <chris@metaversejs.org>
2016-05-03 12:11:25 -0400

    fixes and closes #1

--- config/database.php
+++ config/database.php
@@ -49,7 +49,7 @@ return [
         'sqlite' => [
             'driver'   => 'sqlite',
             'database' => database_path('database.sqlite'),
-            'prefix'   => '',
+            'prefix'   => env('DB_PREFIX', ''),
         ],
 
         'mysql' => [
@@ -60,7 +60,7 @@ return [
             'password'  => env('DB_PASSWORD', ''),
             'charset'   => 'utf8',
             'collation' => 'utf8_unicode_ci',
-            'prefix'    => '',
+            'prefix'    => env('DB_PREFIX', ''),
             'strict'    => false,
         ],
 
@@ -71,7 +71,7 @@ return [
             'username' => env('DB_USERNAME', 'forge'),
             'password' => env('DB_PASSWORD', ''),
             'charset'  => 'utf8',
-            'prefix'   => '',
+            'prefix'   => env('DB_PREFIX', ''),
             'schema'   => 'public',
         ],
 
@@ -82,7 +82,7 @@ return [
             'username' => env('DB_USERNAME', 'forge'),
             'password' => env('DB_PASSWORD', ''),
             'charset'  => 'utf8',
-            'prefix'   => '',
+            'prefix'   => env('DB_PREFIX', ''),
         ],
 
     ],
