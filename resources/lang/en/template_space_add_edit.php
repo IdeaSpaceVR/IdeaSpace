11e8eb2a6c706b674668a35677781183f51b8cf9
Chris Car <chris@metaversejs.org>
2016-06-27 22:49:22 -0400

    localization for space add / edit workflows

---
+++ resources/lang/en/template_space_add_edit.php
@@ -0,0 +1,25 @@
+<?php
+
+return [
+
+    'headline_new_space' => 'Add New Space',
+    'headline_edit_space' => 'Edit Space',
+    'field_errors' => 'There are errors in the fields below.',
+    'space_title_placeholder' => 'Enter title here',
+    'space_uri_placeholder' => 'Enter path here',
+    'publish' => 'Publish',
+    'save_draft' => 'Save Draft',
+    'status' => 'Status:',
+    'edit' => 'Edit',
+    'published' => 'Published',
+    'draft' => 'Draft',
+    'ok' => 'OK',
+    'cancel' => 'Cancel',
+    'publish' => 'Publish',
+    'theme' => 'Theme',
+    'embed_code' => 'Embed Code',
+    'embed_code_help' => 'Copy the code and embed this space on any web site.',
+    'version' => 'Version:',
+    'author' => 'Author:',
+
+];
