6041de1243df1ed8c417a891c03f82adacadcc0d
Chris Car <chris@metaversejs.org>
2016-08-12 23:15:12 -0400

    changed asset library template path

--- resources/views/admin/space/content/field_image_add.blade.php
+++ resources/views/admin/space/content/field_image_add.blade.php
@@ -3,7 +3,7 @@
         <label class="control-label large">{{ $form['#label'] }}</label>
     </div>
     <div class="form-control-add-file text-center {{ $errors->has($field_id)?'has-error':'' }}">
-    @include('admin.assets.open_asset_library', ['field_id' => $field_id, 'form' => $form])
+    @include('admin.asset_library.open_asset_library', ['field_id' => $field_id, 'form' => $form])
     </div>
     <span class="info-block">{{ $form['#help'] }} @foreach ($form['#fileformat'] as $fileformat) <span class="label label-warning">{{ $fileformat }}</span>@endforeach @if ($form['#required'] == false) <span class="label label-success">{{ trans('template_fields.optional') }}</span>@endif</span>
     {!! $errors->has($field_id)?$errors->first($field_id, '<span class="help-block">:message</span>'):'' !!}
