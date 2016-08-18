c0542efee4df94f91ec89ce4294ebe1015fff800
Chris Car <chris@metaversejs.org>
2016-08-17 22:12:16 -0400

    updated assets.js

--- public/assets/admin/asset-library/js/assets.js
+++ public/assets/admin/asset-library/js/assets.js
@@ -1,13 +1,22 @@
 jQuery(document).ready(function($) {
 
+    
+    /* for initial active tab */
+    $('[data-toggle="tooltip"]').tooltip();
+
 
     $('.asset-library-nav a[data-toggle="tab"]').on('show.bs.tab', function (e) {
 
         var currTabTarget = $(e.target).attr('href');
-        var remoteUrl = $(this).attr('data-tab-remote');
+        var remotePageUrl = $(this).attr('data-tab-remote');
+        var remoteScriptUrl = $(this).attr('data-tab-remote-script');
 
-        if (remoteUrl !== '') {                
-            $(currTabTarget).load(remoteUrl);
+        if (remotePageUrl !== '') {                
+            $(currTabTarget).load(remotePageUrl, function() {
+                if (remoteScriptUrl !== '') {
+                    $.getScript(remoteScriptUrl);
+                }
+            });
         } 
     });
 
