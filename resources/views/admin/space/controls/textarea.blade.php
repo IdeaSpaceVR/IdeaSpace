474ea02fdef669bdae39c9cd04d519e0d55bda09
Chris Car <chris@metaversejs.org>
2016-08-23 21:10:30 -0400

    get rid of previous theme api files

--- resources/views/admin/space/controls/textarea.blade.php
+++
@@ -1,5 +0,0 @@
-<div class="form-group <?php echo $control['control_id']; ?>">
-    <label for="textinput"><?php if (array_has($control, 'label')) { echo $control['label']; } ?></label>
-    <textarea class="form-control" rows="5" name="<?php echo $control['control_id']; ?>"><?php echo ((array_has($control, 'data'))?$control['data'][0]['text']:''); ?></textarea>
-    <p class="help-block"><?php if (array_has($control, 'description')) { echo $control['description']; } ?></p>
-</div> <!-- end: form-group //-->
--- resources/views/admin/space/controls/textinput.blade.php
+++
@@ -1,5 +0,0 @@
-<div class="form-group <?php echo $control['control_id']; ?>">
-    <label for="textinput"><?php if (array_has($control, 'label')) { echo $control['label']; } ?></label>
-    <input type="text" name="<?php echo $control['control_id']; ?>" class="form-control" placeholder="Insert text" value="<?php echo ((array_has($control, 'data'))?$control['data'][0]['text']:''); ?>">
-    <p class="help-block"><?php if (array_has($control, 'description')) { echo $control['description']; } ?></p>
-</div> <!-- end: form-group //-->
