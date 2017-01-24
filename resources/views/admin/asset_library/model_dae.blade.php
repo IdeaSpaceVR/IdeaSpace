c016f7185d844c11ae1a3ca611b740e848563c3e
Chris Car <chris@metaversejs.org>
2017-01-23 21:28:14 -0500

    removed 1.6 height from camera wrapper entity as camera height is already set at 1.6

--- resources/views/admin/asset_library/model_dae.blade.php
+++ resources/views/admin/asset_library/model_dae.blade.php
@@ -5,7 +5,7 @@
         <a-asset-item id="model-dae" src="{{ $model_dae }}"></a-asset-item>
     </a-assets>
 
-    <a-entity id="camera" position="0 1.6 4">
+    <a-entity id="camera" position="0 0 4">
         <a-camera></a-camera>
     </a-entity>
 
