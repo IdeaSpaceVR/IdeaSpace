4e4752adf6682d88cc226c647afb64e9e280f0f5
Chris Car <chris@metaversejs.org>
2017-01-30 22:44:25 -0500

    changed reticle

--- resources/views/admin/space/content/field_position/positions_model3d_reticle_partial.blade.php
+++ resources/views/admin/space/content/field_position/positions_model3d_reticle_partial.blade.php
@@ -1,3 +1,7 @@
 <a-ring id="reticle" color="#0080C5" radius-inner="0.08" radius-outer="0.1" visible="false" position="0 0 -1">
-    <a-entity id="reticle-text" position="0.1 0 0" bmfont-text=""></a-entity>
+    <a-plane color="#0080C5" position="0 0.08 0" height="0.1" width="0.03"></a-plane>
+    <a-plane color="#0080C5" position="0.08 0 0" height="0.03" width="0.1"></a-plane>
+    <a-plane color="#0080C5" position="0 -0.08 0" height="0.1" width="0.03"></a-plane>
+    <a-plane color="#0080C5" position="-0.08 0 0" height="0.03" width="0.1"></a-plane>
+    <a-entity id="reticle-text" position="0.15 -0.05 0" bmfont-text=""></a-entity>
 </a-ring>
