8439de5d94f021d6baf63e9339ec22b5e1737632
Chris Car <chris@metaversejs.org>
2017-02-07 22:27:50 -0500

    fixed bug: showed wrong buttons

--- resources/views/admin/space/content/field_position_edit.blade.php
+++ resources/views/admin/space/content/field_position_edit.blade.php
@@ -11,15 +11,15 @@
 
         <input type="hidden" value="{{ $content_id }}" class="content-id">
         <input type="hidden" value="{{ $field_id }}" class="content-key">
-        <input type="hidden" @if (isset($form['#content']['#value'])) value="{{ $form['#content']['#value'] }}" @else value="" @endif name="{{ $field_id }}" class="positions-info">
+        <input type="hidden" @if (isset($form['#content']['#value']) && $form['#content']['#value'] != '') value="{{ $form['#content']['#value'] }}" @else value="" @endif name="{{ $field_id }}" class="positions-info">
 
-        <div class="positions-add" @if (isset($form['#content']['#value'])) style="display:none" @endif>
+        <div class="positions-add" @if (isset($form['#content']['#value']) && $form['#content']['#value'] != '') style="display:none" @endif>
             <button type="button" class="btn btn-primary btn-lg add-positions-btn add-edit-positions-btn" data-space-id="{{ $space_id }}" data-contenttype-name="{{ $contenttype_name }}" data-subject-field-type="{{ $form['#field-type'] }}" data-subject-field-name="{{ $form['#field-name'] }}">
                 <span class="glyphicon glyphicon-plus" aria-hidden="true"></span> {{ trans('fieldtype_position.attach') }} {{ $form['#content-label'] }} 
             </button>
         </div>
 
-        <div class="positions-edit" @if (!isset($form['#content']['#value'])) style="display:none" @endif>
+        <div class="positions-edit" @if (isset($form['#content']['#value']) && $form['#content']['#value'] == '') style="display:none" @endif>
             <div class="positions-placeholder" style="margin-bottom:10px"><i class="fa fa-crosshairs" aria-hidden="true" style="font-size:60px"></i></div>
             <button type="button" class="btn btn-primary add-edit-positions-btn" data-space-id="{{ $space_id }}" data-contenttype-name="{{ $contenttype_name }}" data-subject-field-type="{{ $form['#field-type'] }}" data-subject-field-name="{{ $form['#field-name'] }}">
                 <span class="glyphicon glyphicon-pencil" aria-hidden="true"></span> {{ trans('fieldtype_position.attach') }} &amp; {{ trans('fieldtype_position.detach') }} {{ $form['#content-label'] }}
