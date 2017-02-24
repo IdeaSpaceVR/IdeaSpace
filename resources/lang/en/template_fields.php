efa22dded832d0f75bc748443f030318caf4957b
Chris Car <chris@metaversejs.org>
2017-02-24 18:02:23 -0500

    help text for formatted text fields

--- resources/lang/en/template_fields.php
+++ resources/lang/en/template_fields.php
@@ -7,6 +7,7 @@ return [
     'optional' => 'Optional field',
     'required' => 'Required field',
     'formatted_text' => 'Formatted text',
+    'formatted_text_hint' => 'Select text in order to change format.',
     'plain_text' => 'Plain text',
 
 ];
