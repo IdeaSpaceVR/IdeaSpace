58da19a571346d63714cdff43d5cb66cbf77daca
Wedding DJ <chris@metaversejs.org>
2017-03-23 12:01:17 -0400

    remove old themes

--- themes/photo-sphere-diver/templates/assets.blade.php
+++
@@ -1,5 +0,0 @@
-<a-assets>
-    <img src="{{ url($theme_dir . '/images/loading-sphere.png') }}" id="loading-sphere">
-    <img src="{{ url($theme_dir . '/images/nav-sphere.png') }}" id="nav-sphere">
-</a-assets>
-
