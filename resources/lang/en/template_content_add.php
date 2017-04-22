97e746da670113f5d71f4b6da27cec871ddbc679
Wedding DJ <chris@metaversejs.org>
2017-04-21 23:44:40 -0400

    changed text

--- resources/lang/en/template_content_add.php
+++ resources/lang/en/template_content_add.php
@@ -5,7 +5,7 @@ return [
     'add_new' => 'Add New',
     'field_errors' => 'There are errors in the fields below.',
     'content_title_placeholder' => 'Enter title here',
-    'content_title_info' => 'Enter a descriptive title which makes it easy for you to identify this content on the overview page.',
+    'content_title_info' => 'Enter a descriptive title which makes it easy for you to identify this content.',
     'cancel' => 'Cancel',
     'save' => 'Save',
     'content_uri_placeholder' => 'Enter path here',
