216eafc444312c051b5225878e69fd051d98f40c
Wedding DJ <chris@metaversejs.org>
2017-05-31 12:18:02 -0400

    field type color: set default color to black

--- resources/views/admin/space/content/field_color_add.blade.php
+++ resources/views/admin/space/content/field_color_add.blade.php
@@ -1,7 +1,7 @@
 <div class="form-group {{ $errors->has($field_id)?'has-error':'' }}">
     <label class="control-label large">{{ $form['#label'] }}</label>
     <div class="form-control-color text-center {{ $errors->has($field_id)?'has-error':'' }}">
-        <input type="color" name="{{ $field_id }}" class="form-control input-lg" value="#0099FF">
+        <input type="color" name="{{ $field_id }}" class="form-control input-lg" value="#000000">
     </div>
     <span class="info-block">{{ $form['#help'] }} @if ($form['#required'] == true) <span class="label label-danger">{{ trans('template_fields.required') }}</span>@endif</span>
     {!! $errors->has($field_id)?$errors->first($field_id, '<span class="help-block">:message</span>'):'' !!}
