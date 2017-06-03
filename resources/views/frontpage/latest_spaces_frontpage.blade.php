acd318e8e2f770cf61973f0594762a5323f92ea0
Wedding DJ <chris@metaversejs.org>
2017-06-02 21:47:10 -0400

    500px height for content

--- resources/views/frontpage/latest_spaces_frontpage.blade.php
+++ resources/views/frontpage/latest_spaces_frontpage.blade.php
@@ -12,7 +12,7 @@
 
             <div class="col-sm-8 col-sm-offset-2">
             <h2>{{ $space->title }}</h2>
-            {!! space_embed_code(url($space->uri), '100%', '400px') !!}
+            {!! space_embed_code(url($space->uri), '100%', '500px') !!}
             </div>
 
         </div>
