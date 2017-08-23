be6d84b2d415bb79428a3f36218d073bc973cedf
Wedding DJ <chris@metaversejs.org>
2017-08-23 12:17:14 -0400

    removed unnecessary function

--- themes/ideaspace-3d-model/functions.php
+++ themes/ideaspace-3d-model/functions.php
@@ -1,17 +1,5 @@
 <?php
 
-/**
- * Resize images shown in hotspot annotations.
- */
-Event::listen('ideaspace-model.annotations.image', function($image) {
-
-    /* remember power of two rule for image sizes */    
-    return [
-        'resized-annotation-image' => [
-            'resize' => ['width' => 512, 'height' => null],
-        ]
-    ];
-});
 
 
  
