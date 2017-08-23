ead96bca0d55ccd2ed1e1472d42e402772185b51
Wedding DJ <chris@metaversejs.org>
2017-08-22 23:10:55 -0400

    allow deleting models from asset library which are broken

--- resources/views/admin/asset_library/model_obj_mtl.blade.php
+++ resources/views/admin/asset_library/model_obj_mtl.blade.php
@@ -1,10 +1,12 @@
 <!-- a-frame //-->
 <a-scene reset-camera embedded>
 
+    @if (isset($model_obj) && isset($model_mtl)) 
     <a-assets>
         <a-asset-item id="model-obj" src="{{ $model_obj }}"></a-asset-item>
         <a-asset-item id="model-mtl" src="{{ $model_mtl }}"></a-asset-item>
     </a-assets>
+    @endif
 
     <a-entity id="camera" position="0 0 4">
         <a-camera></a-camera>
@@ -14,6 +16,7 @@
 
     <a-sky color="#000000"></a-sky>
 
+    @if (isset($model_obj) && isset($model_mtl)) 
     <a-entity id="model" scale="{{ $scale }}" position="0 0 0" rotation="{{ $rotation_x }} {{ $rotation_y }} {{ $rotation_z }}" obj-model="obj: #model-obj; mtl: #model-mtl">
         <a-animation
             begin="start-rotation-y"
@@ -25,5 +28,6 @@
             repeat="indefinite">
         </a-animation>
     </a-entity>
+    @endif
 
 </a-scene>
