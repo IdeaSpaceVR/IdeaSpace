5295c3b1b63d03ff1c6e1b09c60ecdb4601ee24f
Wedding DJ <chris@metaversejs.org>
2017-10-06 11:49:32 -0400

    do not show label "Sort content with drag and drop" if there is only one content item allowed

--- resources/views/admin/space/content/contenttypes.blade.php
+++ resources/views/admin/space/content/contenttypes.blade.php
@@ -8,7 +8,7 @@ foreach ($contenttypes as $key => $value) {
   </div>
   <div class="panel-body">
       <button type="button" class="btn btn-primary space-add-content" data-contenttype-key="{{ $key }}" @if ($value['max_values'] != \App\Theme::INFINITE && isset($value['content']) && $value['max_values'] <= count($value['content'])) disabled="disabled" @endif>{{ trans('template_contenttypes.add') }} {{ $value['label'] }}</button>
-      <span class="pull-right" style="margin-top:10px">{{ $value['description'] }} {{ trans('template_contenttypes.drag_n_drop') }}</span>
+      <span class="pull-right" style="margin-top:10px">{{ $value['description'] }} @if ($value['max_values'] > 1 || $value['max_values'] == \App\Theme::INFINITE) {{ trans('template_contenttypes.drag_n_drop') }} @endif</span>
   </div>
 </div>
 
