a661af672b4bbf06ca0bd3db689870b9ab2a769a
Wedding DJ <chris@ideaspacevr.org>
2017-10-16 11:30:53 -0400

    vertically align login/reset password boxes

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -4,7 +4,7 @@
 
 @section('content')
 <div class="container">
-    <div class="row">
+    <div class="row vertical-align">
         <div class="col-md-8 col-md-offset-2">
     
             @if (session('alert-success'))
