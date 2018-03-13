a42594d0060516a1738dbb39807321df9caea8b9
Wedding DJ <chris@ideaspacevr.org>
2018-03-12 23:03:16 -0400

    remove some font styles

--- public/assets/admin/space/content/js/content_add_edit_delete.js
+++ public/assets/admin/space/content/js/content_add_edit_delete.js
@@ -6,7 +6,7 @@ jQuery(document).ready(function($) {
 				plugins: [
     			'autolink lists link charmap textcolor emoticons'
   			],
-				block_formats: 'Paragraph=p;Heading 1=h1;Heading 2=h2;Heading 3=h3;Heading 4=h4;Heading 5=h5;Heading 6=h6',
+				block_formats: 'Paragraph=p;Heading 1=h1;Heading 2=h2;Heading 3=h3',
 				toolbar: 'insert | undo redo |  formatselect fontselect emoticons | bold italic color forecolor backcolor  | alignleft aligncenter alignright alignjustify | bullist outdent indent | removeformat',
 		});
 
@@ -17,7 +17,7 @@ jQuery(document).ready(function($) {
 				plugins: [
     			'autolink lists link charmap textcolor emoticons'
   			],
-				block_formats: 'Paragraph=p;Heading 1=h1;Heading 2=h2;Heading 3=h3;Heading 4=h4;Heading 5=h5;Heading 6=h6',
+				block_formats: 'Paragraph=p;Heading 1=h1;Heading 2=h2;Heading 3=h3',
 				toolbar: 'insert | undo redo |  formatselect fontselect emoticons | bold italic color forecolor backcolor  | alignleft aligncenter alignright alignjustify | bullist outdent indent | removeformat',
 		});
 
