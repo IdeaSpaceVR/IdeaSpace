e7ad3344ec0967ce9586a5e67421b2ac61bbf505
Wedding DJ <chris@ideaspacevr.org>
2018-05-29 12:01:39 -0400

    allow html links in description text

--- resources/views/admin/space/content/contentlist.blade.php
+++ resources/views/admin/space/content/contentlist.blade.php
@@ -11,7 +11,7 @@ foreach ($contentlist as $key => $value) {
 
       <button type="button" class="btn btn-primary space-add-content" data-contenttype-key="{{ $key }}" @if ($value['max_values'] != \App\Theme::INFINITE && isset($value['content']) && $value['max_values'] <= count($value['content'])) disabled="disabled" @endif>{{ trans('template_contentlist.add') }} {{ $value['label'] }}</button>
 
-      <span class="pull-right" style="margin-top:10px">{{ $value['description'] }} @if ($value['max_values'] > 1 || $value['max_values'] == \App\Theme::INFINITE) {{ trans('template_contenttypes.drag_n_drop') }} @endif</span>
+      <span class="pull-right" style="margin-top:10px">{!! $value['description'] !!} @if ($value['max_values'] > 1 || $value['max_values'] == \App\Theme::INFINITE) {{ trans('template_contenttypes.drag_n_drop') }} @endif</span>
 
         <?php
         if (isset($value['content'])) {
