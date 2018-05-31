0fb09c3ebde5b04a43633d9d03f12b662681e15d
Wedding DJ <chris@ideaspacevr.org>
2018-05-31 18:04:47 -0400

    use font loader for google fonts

--- app/Helpers/fonts.php
+++ app/Helpers/fonts.php
@@ -2,5 +2,9 @@
 
 function embed_fonts() {
 
-		echo '<link href="https://fonts.googleapis.com/css?family=Bangers|Bungee+Shade|Codystar|Dr+Sugiyama|Indie+Flower|Lobster|Pacifico|Skranji" rel="stylesheet">';
+		//echo '<link href="https://fonts.googleapis.com/css?family=Bangers|Bungee+Shade|Codystar|Dr+Sugiyama|Indie+Flower|Lobster|Pacifico|Skranji" rel="stylesheet">';
+
+		echo "<link rel=\"preconnect\" href=\"https://fonts.google.com/\" crossorigin>";
+		echo "<script src=\"https://ajax.googleapis.com/ajax/libs/webfont/1.6.26/webfont.js\"></script>";
+		echo "<script>WebFont.load({google:{families: ['Bangers', 'Bungee+Shade', 'Codystar', 'Dr+Sugiyama', 'Indie+Flower', 'Lobster', 'Pacifico', 'Skranji']}});</script>";
 }
