01a435e6b712e770061d3ab40ce8dbda6ff87adc
Wedding DJ <chris@ideaspacevr.org>
2019-01-14 12:48:00 -0500

    heading style change

--- resources/views/admin/space/content/field_group_header.blade.php
+++ resources/views/admin/space/content/field_group_header.blade.php
@@ -1,8 +1,8 @@
 <div class="form-group">
 		<div class="panel panel-default">
-				<div class="panel-heading" role="tab" id="{{ $group_id }}-header">
+				<div class="panel-heading" role="tab" id="{{ $group_id }}-header" style="background-color:#FFFFFF;border-bottom-style:solid;border-bottom-left-radius:4px;border-bottom-right-radius:4px">
 						<h4 class="panel-title">	
-								<a class="collapsed" role="button" data-toggle="collapse" data-parent="#accordion" href="#{{ $group_id }}-collapse" aria-expanded="false" aria-controls="{{ $group_id }}-collapse">
+								<a class="collapsed" style="font-size:18px;color:#0080e5" role="button" data-toggle="collapse" data-parent="#accordion" href="#{{ $group_id }}-collapse" aria-expanded="false" aria-controls="{{ $group_id }}-collapse">
 								<span class="glyphicon glyphicon-menu-down" aria-hidden="true"></span> {{ $form['#title'] }}
 								</a>
 						</h4>
