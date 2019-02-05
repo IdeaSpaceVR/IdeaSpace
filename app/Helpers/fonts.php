e190f333668922d00b17c2ac8f17f458b8bb7584
Wedding DJ <chris@ideaspacevr.org>
2019-02-05 18:45:42 -0500

    allow webfont loader config

--- app/Helpers/fonts.php
+++ app/Helpers/fonts.php
@@ -1,10 +1,12 @@
 <?php
 
-function embed_fonts() {
+function embed_fonts($use_webfont_loader = true) {
 
-		echo '<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Bangers|Bungee+Shade|Codystar|Dr+Sugiyama|Indie+Flower|Lobster|Pacifico|Skranji">';
-
-		//echo "<link rel=\"preconnect\" href=\"https://fonts.google.com/\" crossorigin>";
-		//echo "<script src=\"https://ajax.googleapis.com/ajax/libs/webfont/1.6.26/webfont.js\"></script>";
-		//echo "<script>WebFont.load({google:{families: ['Bangers', 'Bungee+Shade', 'Codystar', 'Dr+Sugiyama', 'Indie+Flower', 'Lobster', 'Pacifico', 'Skranji']}});</script>";
+		if (!$use_webfont_loader) {
+				echo '<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Bangers|Bungee+Shade|Codystar|Dr+Sugiyama|Indie+Flower|Lobster|Pacifico|Skranji">';
+		}	else {
+				echo "<link rel=\"preconnect\" href=\"https://fonts.google.com/\" crossorigin>";
+				echo "<script src=\"https://ajax.googleapis.com/ajax/libs/webfont/1.6.26/webfont.js\"></script>";
+				echo "<script>WebFont.load({google:{families: ['Bangers', 'Bungee Shade', 'Codystar', 'Dr Sugiyama', 'Indie Flower', 'Lobster', 'Pacifico', 'Skranji']}});</script>";
+		}
 }
