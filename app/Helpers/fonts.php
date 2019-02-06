63b66e01e7fd8b25e2b695ec41f25389a1d70beb
Wedding DJ <chris@ideaspacevr.org>
2019-02-06 12:13:17 -0500

    cleanup

--- app/Helpers/fonts.php
+++ app/Helpers/fonts.php
@@ -5,8 +5,8 @@ function embed_fonts($use_webfont_loader = true) {
 		if (!$use_webfont_loader) {
 				echo '<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Bangers|Bungee+Shade|Codystar|Dr+Sugiyama|Indie+Flower|Lobster|Pacifico|Skranji">';
 		}	else {
-				echo "<link rel=\"preconnect\" href=\"https://fonts.google.com/\" crossorigin>";
-				echo "<script src=\"https://ajax.googleapis.com/ajax/libs/webfont/1.6.26/webfont.js\"></script>";
-				echo "<script>WebFont.load({google:{families: ['Bangers', 'Bungee Shade', 'Codystar', 'Dr Sugiyama', 'Indie Flower', 'Lobster', 'Pacifico', 'Skranji']}});</script>";
+				echo '<link rel="preconnect\" href="https://fonts.google.com/" crossorigin>';
+				echo '<script src="https://ajax.googleapis.com/ajax/libs/webfont/1.6.26/webfont.js"></script>';
+				echo '<script>WebFont.load({google:{families: ["Bangers", "Bungee Shade", "Codystar", "Dr Sugiyama", "Indie Flower", "Lobster", "Pacifico", "Skranji"]}});</script>';
 		}
 }
