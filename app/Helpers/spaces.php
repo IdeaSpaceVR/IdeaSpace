7e5cd169815ca4c77a128911be34b23bb6762882
Wedding DJ <chris@ideaspacevr.org>
2020-01-14 19:59:21 +0000

    added allow="xr-spatial-tracking" to iframe

--- app/Helpers/spaces.php
+++ app/Helpers/spaces.php
@@ -6,5 +6,5 @@ function space_embed_code($space_url, $width, $height = null) {
     if ($height != null) {
       $height_html = 'height=' . $height; 
     }
-    return '<iframe width="' . $width . '" ' . $height_html . ' allowfullscreen frameborder="0" src="' . $space_url . '"></iframe>';
+    return '<iframe allow="xr-spatial-tracking" width="' . $width . '" ' . $height_html . ' allowfullscreen frameborder="0" src="' . $space_url . '"></iframe>';
 }
